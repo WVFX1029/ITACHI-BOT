5c1e377d430dea87370382e42a38a9b5d31a0019
ABU <98000024+Ajx-Abu@users.noreply.github.com>
2022-02-24 21:02:20 +0530

    Update StringSession.js

--- Abu/StringSession.js
+++ Abu/StringSession.js
@@ -18,7 +18,7 @@ class StringSession {
 
     deCrypt(string = undefined) {
 
-        if ('ABUSER_CODE' in process.env && string === undefined) {
+        if ('ITACHI_CODE' in process.env && string === undefined) {
 
             string = process.env.STRING_SESSION;
 
